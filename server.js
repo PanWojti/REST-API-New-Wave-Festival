3d2cc612777b0583ae5ec705782b8363acbc9622
Wojciech Sitkiewicz <sitkiewicz.wojciech@gmail.com>
2020-04-14 19:59:57 +0200

    Add urlencoded, json and cors middleware

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
 const express = require('express');
 const path = require('path');
+const cors = require('cors');
 
 const app = express();
+
+app.use(cors());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 
 app.listen(8000, () => {
   console.log('Server is running on port: 8000');
