0f03c75e05392f83dfc5f6fd5ddeea6ab8ce54a5
Wojciech Sitkiewicz <sitkiewicz.wojciech@gmail.com>
2020-04-19 17:49:23 +0200

    Remove build from gitignore

---
+++ client/build/precache-manifest.87e299d1905cb4a398c2f235fac45181.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2653b24f8c97604a60804ab74ddc30f3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b94727c6ce93cd82460c",
+    "url": "/static/css/2.cd5b1545.chunk.css"
+  },
+  {
+    "revision": "89e6642c2acb1e7902dc",
+    "url": "/static/css/main.8f9b5a57.chunk.css"
+  },
+  {
+    "revision": "b94727c6ce93cd82460c",
+    "url": "/static/js/2.c9e4ea83.chunk.js"
+  },
+  {
+    "revision": "3d66a4ed243bca57a1e68168bbc08249",
+    "url": "/static/js/2.c9e4ea83.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "89e6642c2acb1e7902dc",
+    "url": "/static/js/main.6f3add3b.chunk.js"
+  },
+  {
+    "revision": "702554416a81a1e3a894",
+    "url": "/static/js/runtime-main.784c586e.js"
+  }
+]);
